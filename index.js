1c41d728fca48ccf96716f7f60f6ea3788253c71
Javier R <javierresines@gmail.com>
2022-06-11 20:00:28 -0500

    working on main function to call first inquirer

--- index.js
+++ index.js
@@ -12,3 +12,73 @@ const db = mysql.createConnection({
     password: "1266",
     database: "maindatabase"
 });
+
+function prqu() {
+
+    inquirer
+
+    .prompt({
+        name:"first",
+        type:"list",
+        message: "select activity",
+        choices:[
+            "view all departments",
+            "view all roles",
+            "view all employees",
+            "add new role",
+            "add new employee",
+            "update existing employee's role",
+        ],
+    })
+
+    .then(function(answer){
+        switch(answer.first){
+            case "view all departments":
+
+                viewalldepartments();
+
+                break;
+    
+            case "view all roles":
+
+                viewallroles();
+
+                break;
+            
+            case "view all employees":
+
+                viewallemployees();
+
+                break;
+            
+            case "add new employee":
+                
+                addnewemployee();
+
+                break;
+
+            case "add new role":
+
+                addnewrole();
+
+                break;
+            
+            case "add new department":
+            
+               addnewdepartment();
+               
+               break;
+            
+            case "update existing employee's role":
+
+               updateemployee();
+               
+               break;
+            
+
+        
+
+        }
+    });
+
+}
