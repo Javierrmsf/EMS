f12d277eb2d6c7bd6f166e5c6eeefc1ec42b6332
Javier R <javierresines@gmail.com>
2022-06-11 20:53:32 -0500

    update employee function working in this version and troubleshooting

--- index.js
+++ index.js
@@ -127,24 +127,136 @@ function viewalldepartments() {
         {
           name: "departmentname",
           type: "input",
-          message: "enter the department name",
+          message: "enter the department's name",
         },
       ])
       .then(function (answer) {
         var query = "INSERT INTO department (name) VALUE (?)";
         db.query(query, answer.departmentname, function (err, res) {
           if (err) throw err;
-
           console.log("Success");
-          
           prqu();
         });
       });
   }
 
+  function addnewrole() {
+    inquirer
+      .prompt([
+        {
+          name: "title",
+          type: "input",
+          message: "Enter the role's title",
+        },
+        {
+          name: "salary",
+          type: "input",
+          message: "enter the role's salary",
+        },
+        {
+          name: "department",
+          type: "input",
+          message:
+            "Enter the department ID for this role, make sure the Department ID already exists",
+          
+        },
+      ])
+      .then(function (answer) {
+        var query =
+          "INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)";
+        db.query(
+          query,
+          [answer.title, answer.salary, answer.department],
+          function (err, res) {
+            if (err) throw err;
+              console.log("Success");
+              prqu();
+            }
+        )}
+      )}
 
-
-
+      function addnewemployee() {
+        inquirer
+          .prompt([
+            {
+              name: "name",
+              type: "input",
+              message: "Enter the employee's first name",
+            },
+            {
+              name: "lastname",
+              type: "input",
+              message: "Enter the employee's last name",
+            },
+            {
+              name: "roleid",
+              type: "input",
+              message: "enter the employee's role id",
+            },
+            {
+              name: "managerid",
+              type: "input",
+              message: "enter the employee's manager's id",
+            },
+          ])
+          .then(function (answer) {
+            var query =
+              "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
+            db.query(
+    
+              query,
+              
+              [answer.name, answer.lastname, answer.roleid, answer.managerid],
+    
+              function (err, res) {
+                if (err) throw err;
+                  console.log("success");
+                  prqu();
+              }
+    
+            );
+          });
+      }
+    
+      function updateemployee() {
+        inquirer
+          .prompt([
+            {
+              name: "employeeid",
+              type: "input",
+              message: "enter the id of the employee you would like to update",
+            },
+            {
+              name: "newrole",
+              type: "input",
+              message: "enter the name of the employee's new role",
+            },
+            {
+              name: "salary",
+              type: "input",
+              message: "enter salary for this position",
+            },
+            {
+              name: "roleid",
+              type: "input",
+              message: "enter the department number ID",
+            },
+          ])
+          .then(function(answer) {
+            var query = "UPDATE roles SET title = ?, salary = ?, department_id = ? WHERE id = ?";
+    
+    
+            /////////////////////////////////////
+                db.query(query, [answer.newrole, answer.salary, answer.roleid, parseInt(answer.employeeid)], function(err, res) {
+                  if (err) throw (err);
+    
+    
+                  console.log("Success");
+                  prqu();
+                  })
+              }
+      )}
+    
 
 
 
